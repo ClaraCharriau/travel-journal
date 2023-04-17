29327853f5077019088ce3003886858ade7fcff2
Clara Charriau <charriau.clara@gmail.com>
2023-04-17 12:43:35 +0200

    [FEATURE] Creation home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './general/header/header.component';
 import { FooterComponent } from './general/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/post/post.component';
+import { LastPostComponent } from './home/last-post/last-post.component';
+import { OtherPostsComponent } from './home/other-posts/other-posts.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { PostComponent } from './pages/post/post.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    PostComponent
+    PostComponent,
+    LastPostComponent,
+    OtherPostsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/other-posts/other-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-posts',
+  templateUrl: './other-posts.component.html',
+  styleUrls: ['./other-posts.component.css']
+})
+export class OtherPostsComponent {
+
+}
