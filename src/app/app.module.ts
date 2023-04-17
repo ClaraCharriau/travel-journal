57d44266cb89e4cede447fd61860f8b5ec402e5c
Clara Charriau <charriau.clara@gmail.com>
2023-04-17 11:56:47 +0200

    [FEATURE] Homepage and Postpage creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './general/header/header.component';
+import { FooterComponent } from './general/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
